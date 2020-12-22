20e3b2968b4fb0226d060fff470f9cf65a824fe5
Dennis Wendland <dennis.wendland@fiware.org>
2020-12-22 12:26:02 +0100

    Fix Swagger UI page

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
+import os
 from flask_restplus import Api
-from flask import Blueprint
+from flask import Blueprint, url_for
 
 from .main.controller.data_service_controller import api as data_service_ns
 
 blueprint = Blueprint('api', __name__)
+
+if os.getenv('HTTPS_SCHEME', True):
+    @property
+    def specs_url(self):
+        return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+ 
+    Api.specs_url = specs_url
+    
 
 api = Api(blueprint,
           title='Marketplace Activation Service API',
